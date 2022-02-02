50e1c9c0e821fc3d91d3107d8e74eb326ea351d3
anandbaraik <anandbaraik2014@gmail.com>
2022-02-02 23:02:41 +0530

    implementation: question & money pyramid ui :white_check_mark:

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <div className="main">
+        quiz
+      </div>
+      <div className="pyramid">
+        <ul className="moneyList">
+          <li className="moneyListItem active">
+            <span className="moneyListNumber">
+              4
+            </span>
+            <span className="moneyListAmount">
+              $4000
+            </span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
